7ab3e3360d46eadae144b55867ebf54d416d95bd
Erick Rodríguez <e12antonio@gmail.com>
2023-10-15 13:19:35 -0600

    project finished

---
+++ src/calculadora/contable/java/CalculadoraContableJava.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calculadora.contable.java;
+import calculadora.contable.java.MainMenu;
+/**
+ *
+ * @author e12an
+ */
+public class CalculadoraContableJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MainMenu frame = new MainMenu();
+        frame.setVisible(true);
+    }
+    
+}
